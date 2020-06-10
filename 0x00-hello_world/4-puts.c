73d91e9f270cb0c65e8e748224ec13fae3037d2f
Blake Stewart <lee.stewart909@gmail.com>
2020-06-10 20:36:38 +0000

    created/betty 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
-/*
-* main - Entry point
-* this program will output "Programming is like building a multilingual puzzle
-* Via puts
-* Return 0
+/**
+* main - entry point
+*
+* Return: returns 0(success)
 */
-
 int main(void)
 {
-puts("\"Programming is like building a multiligual puzzle");
-return (0);
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
 }
