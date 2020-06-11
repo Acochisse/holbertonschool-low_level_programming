c39b0808df87e93789e9d0b5b49d46ae35eb901b
Blake Stewart <lee.stewart909@gmail.com>
2020-06-11 18:35:07 +0000

    update redux 1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,15 +18,15 @@ int main(void)
 	x = n % 10;
 
 	printf("last digit of %i is %i ", n, x);
-	if (n > 5)
+	if (x > 5)
 	{
 		printf("and is greater than 5\n");
 	}
-	else if (n < 6 && n != 0)
+	else if (x < 6 && x != 0)
 	{
 		printf("and is less than 6 and not 0\n");
 	}
-	else if (n == 0)
+	else
 	{
 		printf("and is 0\n");
 	}
