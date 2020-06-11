1fb689b94a76e77991a2cd6a3e260220c3120850
Blake Stewart <lee.stewart909@gmail.com>
2020-06-11 20:31:44 +0000

    created/betty/gcc 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This program prints all single d # b10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%i", x);
+	putchar('\n');
+	return (0);
+}
