4bb4e5d7f78dde6cb11c38b0a90d7cefd923478a
Blake Stewart <lee.stewart909@gmail.com>
2020-06-11 22:00:23 +0000

    created/betty/gcc/tested 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* this program prints z-a
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 122; x > 96; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
