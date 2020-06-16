5f17b2e623f00c38be160574783a0d8ce774d997
Blake Stewart <lee.stewart909@gmail.com>
2020-06-16 20:12:23 +0000

    created holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+* File: holberton.h
+*
+* Desc: Header file containing declarations for all functions
+*       used in the 0x02-functions_nested_loops directory.
+*/
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+
+
+#endif
