1fe9d7cd466eea6b0ae814da36696be49bef7653
Blake Stewart <lee.stewart909@gmail.com>
2020-06-18 17:38:10 +0000

    created and tested 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * int mul - multiplies two integers
+ * @a : a side
+ * @b : b side
+ * Return: returns sum of equation
+ *
+ */
+
+int mul(int a, int b)
+
+{
+
+	long int mult;
+
+	mult = (a * b);
+
+	return (mult);
+
+}
