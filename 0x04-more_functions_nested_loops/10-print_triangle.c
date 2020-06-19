6477a216ced84e9675536c011c76fe2486dfe33c
Blake Stewart <lee.stewart909@gmail.com>
2020-06-19 00:44:54 +0000

    created and tested 10-print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: input size
+ * Return: nothing
+ *
+ */
+
+void print_triangle(int size)
+
+{
+	int x;
+	int y;
+	int z;
+
+	if (size <= 0)
+
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				z = size - x - 1;
+				if (y < z)
+					_putchar(' ');
+				else
+					_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
