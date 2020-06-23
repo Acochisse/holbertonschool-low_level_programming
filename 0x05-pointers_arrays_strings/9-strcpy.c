2dead86d5cab890f0d0b945b39b3bd130d65d1ec
Acochisse <lee.stewart909@gmail.com>
2020-06-23 13:11:54 -0500

    created and tested 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies string to a destination
+ * @dest: destination
+ * @src: input
+ *
+ * Return: point to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; (dest[i] = src[i]) != '\0'; i++)
+		     ;
+	return (dest);
+}
