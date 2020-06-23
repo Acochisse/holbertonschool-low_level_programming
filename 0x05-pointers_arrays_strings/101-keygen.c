70965d47739ee5f2f2f52553f3d9032acc5a4142
Acochisse <lee.stewart909@gmail.com>
2020-06-23 15:22:25 -0500

    created and tested

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+	int r;
+	int sum;
+	char array[63] = "abcdefghijkmnopqrstuwvxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char password[100];
+
+	srand(time(NULL));
+	sum = 0;
+	x = 0;
+	while (sum < (2772 -122))
+	{
+		r = rand() % 62;
+		password[x] = array[r];
+		sum = sum + password[x];
+		x++;
+	}
+	r = 2772 - sum;
+	password[x] = r;
+	x++;
+	password[x] = '\0';
+	printf("%s", password);
+
+	return (0);
+}
