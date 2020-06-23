09ad65d3050c0f0cdb2eaee80b1b35ad3bee67f9
Acochisse <lee.stewart909@gmail.com>
2020-06-23 09:23:46 -0500

    created and tested 2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _strlen - return length string
+* @s: target string
+*
+* Return: length value
+*
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	if (s)
+	{
+		while (*(s + length));
+	}
+	return (length);
+}
