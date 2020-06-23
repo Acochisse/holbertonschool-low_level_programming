6fd3dd5ceef9e32c6f10aa284767c72cb643ca49
Acochisse <lee.stewart909@gmail.com>
2020-06-23 09:43:39 -0500

    created and tested 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _puts - prints the string *str
+* @str: imput string
+*
+* Return: nothing
+*
+*/
+
+void _puts(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str[len] != '\0')
+	{
+		_putchar(*str[len]);
+	}
+	_putchar('\n');
+}
