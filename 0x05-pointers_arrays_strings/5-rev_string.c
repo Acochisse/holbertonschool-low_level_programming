15a60c630b6cc02106feeeb17ad03127ebccb110
Acochisse <lee.stewart909@gmail.com>
2020-06-23 11:03:43 -0500

    created and tested 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a pointed string
+ * @s: input string
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i;
+	int x;
+	int len;
+	char tmp;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	len = i;
+	i--;
+
+	for (x = 0; x < len / 2; x++)
+	{
+		tmp = s[x];
+		s[x] = s[x];
+		s[i] = tmp;
+		i--;
+	}
+}
