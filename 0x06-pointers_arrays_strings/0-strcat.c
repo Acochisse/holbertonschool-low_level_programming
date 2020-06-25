d488956029e0de4912ffde0a7b02dd8640f76f04
Acochisse <lee.stewart909@gmail.com>
2020-06-25 11:00:17 -0500

    created and tested 0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+/**
+* _strlen - return length string
+* @s: target string
+*
+* Return: length value
+*
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++);
+
+	return (len);
+}
+
+#include "holberton.h"
+/**
+* _strcat - Convolves two strings
+* @dest: output string
+* @src: input string
+*
+* Return: pointer to destination
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int len = _strlen(dest);
+
+	for (i = 0; src[i] != '\0'; i++)
+
+		dest[len++] = src[i];
+
+	return (dest);
+}
