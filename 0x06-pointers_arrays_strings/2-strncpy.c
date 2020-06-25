b86c9ecf4a6c06928344d744f117e10fddb0981d
Acochisse <lee.stewart909@gmail.com>
2020-06-25 12:23:49 -0500

    created and tested 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,54 @@
+/**
+* _strlen - return length string
+* @s: target string
+*
+* Return: length value
+*
+*/
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+#include "holberton.h"
+/**
+* _strcat - Convolves two strings
+* @dest: output string
+* @src: input string
+*
+* Return: pointer to destination
+*/
+
+#include "holberton.h"
+
+/**
+*_strncpy - copies string
+*@src:  Pointer to input string
+*@dest: Pointer to output string
+*@n: Bytesize of src
+*
+* Return: Pointer to destination
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i;
+	int lensrc = _strlen(src);
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i >= lensrc)
+
+			dest[i] = '\0';
+
+		else
+
+			dest[i] = src[i];
+	}
+	return (dest);
+}
