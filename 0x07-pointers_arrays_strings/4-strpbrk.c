bc93b39d25a672261dcd0f54200e8f8ec1b5fc75
Acochisse <lee.stewart909@gmail.com>
2020-06-29 11:52:39 -0500

    created and tested

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * strpbrk - searches a string of any of a set of bytes
+ * @s: first string
+ * @accept: second string
+ * Return: pointer to byte in s that matches
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+	unsigned int i;
+	unsigned int x;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (x = 0; *(accept + x) != '\0'; x++)
+		{
+			if (*(s + i) == *(accept + x))
+				return (s + i);
+		}
+	}
+	return ('\0');
+}
