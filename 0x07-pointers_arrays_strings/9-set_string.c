65e3ec26aee4d329b0177eb165d9fb81c32f6bb8
Acochisse <lee.stewart909@gmail.com>
2020-06-29 14:07:36 -0500

    created and tested 9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - set pointer to char
+ * @s: input pointer
+ * @to: dest pointer
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
