8c6800d6459d4d7e7f94c9017555711f6f3aaee6
Acochisse <lee.stewart909@gmail.com>
2020-06-29 11:12:46 -0500

    created and tested 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copies memory block
+ * @dest: output memory block
+ * @src: input memory block
+ * @n: bytes filled
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i< n; n++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
