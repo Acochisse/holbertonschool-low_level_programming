ab1d545c3db0536afb29d7a42fd002b5db9d3af0
Acochisse <lee.stewart909@gmail.com>
2020-06-30 09:55:57 -0500

    fixed

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,14 +11,14 @@
 char *_strpbrk(char *s, char *accept)
 
 {
-	unsigned int i;
-	unsigned int x;
+	int i;
+	int x;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (x = 0; *(accept + x) != '\0'; x++)
+		for (x = 0; accept[x] != '\0'; x++)
 		{
-			if (*(s + i) == *(accept + x))
+			if (s[i] == accept[x])
 				return (s + i);
 		}
 	}
