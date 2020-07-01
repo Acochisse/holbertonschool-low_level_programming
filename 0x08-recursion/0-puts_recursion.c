a62b5d5607f71954972cf27f4f3a1f9c3c003649
Acochisse <lee.stewart909@gmail.com>
2020-07-01 12:55:28 -0500

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,14 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == ('\0'))
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(s[i]);
-	s++;
-	_puts_recursion(s);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
