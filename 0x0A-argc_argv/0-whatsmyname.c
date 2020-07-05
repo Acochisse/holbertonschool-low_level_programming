09273630744f2541b9f6c4afe8ba3648970290de
Acochisse <lee.stewart909@gmail.com>
2020-07-05 13:14:09 -0500

    created README.md

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function with 2 arguments
+ * @argc: int arg
+ * @argv: pointer array arg
+ *
+ * Return: void
+ *
+ */
+int main(int argc, char *argv[])
+
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+This directory is for files related to project 221 0x0A-argv_argc
+
