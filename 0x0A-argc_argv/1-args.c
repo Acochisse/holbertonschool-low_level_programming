a60b7025cb0ce49144ad2122cdc7d1cbf8baf29b
Acochisse <lee.stewart909@gmail.com>
2020-07-05 13:33:00 -0500

    created and tested 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function with 2 args
+ * @argc: int arg
+ * @argc: pointer array arg
+ *
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+
+{
+	(void)argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
