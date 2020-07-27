e59ef3024f7dfc048c4476cf5cb7eca40e753872
Acochisse <1819@holbertonschool.com>
2020-07-27 17:10:16 -0500

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,17 +13,12 @@ void print_alphabet_x10(void)
 	int x;
 
 	for (lc = 0; lc < 10; lc++)
-
 	{
-
-		for (x = 97; x < 122; x++)
-
+		for (x = 97; x <= 121; x++)
 		{
-
 			_putchar(x);
-
 		}
-
 		_putchar('\n');
-	}
+       	}
+
 }
