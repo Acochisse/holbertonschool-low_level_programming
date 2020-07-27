956d16c6ffa639b39ab2be5602266ca6bf4b114f
Acochisse <1819@holbertonschool.com>
2020-07-27 14:21:19 -0500

    created and tested 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints number of elements in a linked list
+ * @h: const pointer
+ *
+ *
+ * Return: number of nodes
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+
+{
+	int c = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d]\n", h->n);
+		c += 1;
+		h = h->next;
+	}
+	return (c);
+}
