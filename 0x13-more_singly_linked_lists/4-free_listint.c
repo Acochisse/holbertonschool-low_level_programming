1a36ee7de023efe6f7d15c30fbee735d6783b394
Acochisse <1819@holbertonschool.com>
2020-07-27 15:19:18 -0500

    created and tested

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the current listint_t
+ * @head: double pointer to list
+ *
+ *
+ * Return: none
+ *
+ */
+
+void free_listint(listint_t *head)
+
+{
+	listint_t *index = head;
+	listint_t *end_node;
+	while (index)
+	{
+		end_node = index->next;
+		free(index);
+  		index = end_node;
+	}
+}
