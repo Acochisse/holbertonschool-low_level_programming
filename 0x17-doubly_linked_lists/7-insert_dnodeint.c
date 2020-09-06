12ca24dbd042ec08648ba50ae0e04e349724a54e
1819@holbertonschool.com <1819@holbertonschool.com>
2020-09-06 10:58:11 -0500

    updated parentheses

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,8 +22,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new->next = *h;
-		*h->prev = new;
-		*h = new;
+		(*h)->prev = new;
+		(*h) = new;
 		return (new);
 	}
 	else
