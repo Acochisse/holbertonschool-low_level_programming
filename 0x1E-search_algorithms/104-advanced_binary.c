4b4d2f3459da95e4895bae384d1eb9ae436d894e
Acochisse <1819@holbertonschool.com>
2022-03-24 11:38:42 -0500

    adding 104

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+
+/**
+* advanced_binary_recursive - recursive function that binary searches with one loop recursively
+* @array: array to be searched
+* @size: size of the array
+* @value: target value
+* Return: positon, otherwise -1
+**/
+
+int advanced_binary_recursive(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (advanced_binary_recursive(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (advanced_binary_recursive(array, half + 1, value));
+
+	half++;
+	return (advanced_binary_recursive(array + half, size - half, value) + half);
+}
+
+/**
+* advanced_binary -driver
+* @array: array to be searched
+* @size: size of the array
+* @value: target value
+* Return: positon, otherwise -1
+**/
+int advanced_binary(int *array, size_t size, int value)
+{
+	int index;
+
+	index = advanced_binary_recursive(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
+}
